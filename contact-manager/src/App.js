c55e835f5db824808bd4fceee5f9d5fca4a1af3d
dewi-xaltius <smdewi@xaltius.tech>
2024-05-22 22:27:38 +0800

    Contact Manager App

---
+++ contact-manager/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import ContactManager from './components/ContactManager';
+
+const App = () => {
+    return (
+        <div className="App">
+            <ContactManager />
+        </div>
+    );
+};
+
+export default App;
