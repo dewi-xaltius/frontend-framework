49e7826594e526eb3226a5c737cb8d84af60e370
dewi-xaltius <smdewi@xaltius.tech>
2024-05-24 16:09:26 +0800

    modified UserDetails to show user.name in the alert box

--- user-management-dashboard/src/components/UserDetails.js
+++ user-management-dashboard/src/components/UserDetails.js
@@ -8,7 +8,7 @@ const UserDetails = ({ user }) => {
       <h2>{user.name}</h2>
       <p>Email: {user.email}</p>
       <p>Phone: {user.phone}</p>
-      <button onClick={() => alert('More details coming soon!')}>More Info</button>
+      <button onClick={() => alert(`More details about ${user.name}coming soon!`)}>More Info</button>
     </div>,
     document.getElementById('modal-root')
   );
